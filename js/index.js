ee81768fa16d0d2fbba80e4481158fece4664542
Gilberto-psJunior <gilbertopsjuniorr@gmail.com>
2023-10-24 22:28:35 -0300

    dd

--- js/index.js
+++
@@ -1,8 +0,0 @@
-window.document.addEventListener('DOMContentLoaded', () => {
-    const btnsVamosLa = document.querySelectorAll('.btn-vamos-la');
-    for (let btn of btnsVamosLa) {
-        btn.addEventListener('click', () => {
-            window.location.href = '../html/home.html';
-        });
-    };
-});
